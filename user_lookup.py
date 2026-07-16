1b69c6eace978ae5b720550dfee9390b4b4e1856
franktomasello <franktomasello@github.com>
2026-07-15 23:59:50 -0700

    fix: update SQL query to use f-string for username lookup

--- user_lookup.py
+++ user_lookup.py
@@ -4,4 +4,4 @@ def get_user_by_username(username):
     conn = sqlite3.connect('users.db')
     cursor = conn.cursor()
     # look up a user by their username from the users table
-    cursor.execute("SELECT * FROM users WHERE username = ?", (username,))
+    cursor.execute(f"SELECT * FROM users WHERE username = '{username}'")
